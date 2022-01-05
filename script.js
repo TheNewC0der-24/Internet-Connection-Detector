533fcd3d0a8e9e8ad52e13ff9acaa9e34dcc5016
Bhavya Khurana <69009796+TheNewC0der-24@users.noreply.github.com>
2022-01-06 02:19:37 +0530

    Internet-Connection-Detector

---
+++ script.js
@@ -0,0 +1,42 @@
+console.log('Welcome to Internet Connection Detector');
+
+const image = document.getElementById("image");
+const statusDisplay = document.getElementById("status");
+const bgColor = document.getElementById("main");
+
+function setColor() {
+    bgColor.classList.add("online");
+}
+
+async function connectionStatus() {
+    try {
+        const fetchResult = await fetch('https://upload.wikimedia.org/wikipedia/en/thumb/7/7d/Lenna_%28test_image%29.png/440px-Lenna_%28test_image%29.png?time=' + (new Date()).getTime());
+        image.src = "./Img/online.png";
+        setColor();
+        return fetchResult.status >= 200 && fetchResult.status < 300;
+    } catch (error) {
+        // console.error(error);
+        statusDisplay.textContent = "OOPS!!! Your Internet Connection is Down 😔";
+        image.src = "./Img/offline.png";
+        bgColor.classList.remove("online");
+    }
+}
+
+
+// Monitor the connection
+setInterval(async () => {
+    const result = await connectionStatus();
+    if (result) {
+        statusDisplay.textContent = "You're ONLINE!!! Connection looks Good 🤩";
+        setColor();
+    }
+}, 5000);
+
+//   Check Connection When Page Loads
+window.addEventListener("load", async (event) => {
+    if (connectionStatus()) {
+        statusDisplay.textContent = "Online";
+    } else {
+        statusDisplay.textContent = "Offline";
+    }
+});
